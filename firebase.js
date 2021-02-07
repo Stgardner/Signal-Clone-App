034b5bce01d5b7825fa49b67534420e07ccd333e
Stuart Gardner <stuarttgardner@gmail.com>
2021-02-07 16:11:28 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,15 +3,6 @@ import "firebase/auth";
 import "firebase/firestore";
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCOznWz6hLPadwbuYbefPVhfq607_q08JU",
-    authDomain: "signal-clone-aa99d.firebaseapp.com",
-    projectId: "signal-clone-aa99d",
-    storageBucket: "signal-clone-aa99d.appspot.com",
-    messagingSenderId: "1033670331928",
-    appId: "1:1033670331928:web:8b3362e584d739a362fd6d"
-  };
-
   let app;
 
   if (firebase.apps.length=== 0 ) {
