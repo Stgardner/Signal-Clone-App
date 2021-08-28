d2da8b603065f4d9cf9a8ddb1db7a9d142cdbcc2
Stuart Gardner <stuarttgardner@gmail.com>
2021-08-28 09:03:56 -0700

    firebas config

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
-import HomeScreen from "./screens/HomeScreen"
+
 
 const Stack = createStackNavigator();
 
@@ -22,7 +22,6 @@ export default function App() {
       <Stack.Navigator screenOptions={globalScreenOptions}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- firebase.js
+++ firebase.js
@@ -1,9 +1,18 @@
 import * as firebase from "firebase";
 import "firebase/auth";
 import "firebase/firestore";
+import { initializeApp } from "firebase/app";
 
 //Firebase config goes here
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCOznWz6hLPadwbuYbefPVhfq607_q08JU",
+  authDomain: "signal-clone-aa99d.firebaseapp.com",
+  projectId: "signal-clone-aa99d",
+  storageBucket: "signal-clone-aa99d.appspot.com",
+  messagingSenderId: "1033670331928",
+  appId: "1:1033670331928:web:8b3362e584d739a362fd6d"
+};
   let app;
 
   if (firebase.apps.length=== 0 ) {
